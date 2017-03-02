f3e6e917b416c80f9315c26d99c24333ed7d0b7b
Marko McDowell <marko.mcdowell@gmail.com>
2017-03-02 09:07:53 -0700

    Delete CombatEncounter.java

--- Code/Encounters/CombatEncounter.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class CombatEncounter extends EncounterClass
-{
-	protected String[] consequence = new String[1];
-	protected String[] enemies = new String[1];
-	
-	public abstract void doCombat();
-}
